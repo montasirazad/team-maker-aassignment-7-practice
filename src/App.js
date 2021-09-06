9d2647a93fb9ddb3fec773e13204bb598b75e6bc
unknown <montasirazad@gmail.com>
2021-09-06 14:04:54 +0600

    data loaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+ import { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
 import './App.css';
-
+import fakeData  from '../src/Fake Data/fakeData.json'
 function App() {
+  const [player , setPlayer ] = useState([])
+  console.log(player)
+  
+  useEffect(()=>{
+    setPlayer(fakeData)
+    
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+
+       
     </div>
   );
 }
